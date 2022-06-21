1d9a66ae353a66534b9ae3c9e8d19baf1c3d7a80
JuanOssa1 <juan.ossa2@u.icesi.edu.co>
2022-06-21 17:10:45 -0500

    url bug fix

--- diaPadreScrapper/diaPadreScrapper/spiders/diaPadre.py
+++ diaPadreScrapper/diaPadreScrapper/spiders/diaPadre.py
@@ -2,18 +2,18 @@ import scrapy
 
 class DiapadreSpider(scrapy.Spider):
     name = 'diaPadre'
-    allowed_domains = ['listado.mercadolibre.com.co/_Deal_dia-del-padre-2022']
-    start_urls = ['https://listado.mercadolibre.com.co/_Deal_dia-del-padre-2022/']
+    start_urls = ['https://listado.mercadolibre.com.co/_Deal_dia-del-padre-2022']
 
     def parse(self, response):
-        nombre= response.css(".ui-search-item__title ui-search-item__group__element::text").extract()
-        dataSearched = {"a"}
-        #precio=
-        #descuento=
-        for oferta in zip(nombre):
-            dataSearched = {
-                'title' : oferta[0]
+        #products= response.xpath("//h2[@class='ui-search-item__title']").extract()
+        products= response.css(".ui-search-item__title.ui-search-item__group__element::text").extract()
+        prices= response.css(".price-tag-fraction::text").extract()
+        
+
+        for searched in zip(products,prices):
+            searchedInfo={
+                'product': searched[0],
+                'price': searched[1]
             }
-        print("Hola")
-        print(dataSearched)
-        yield dataSearched
+
+            yield searchedInfo
