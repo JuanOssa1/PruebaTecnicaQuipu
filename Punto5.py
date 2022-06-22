d6b154b92562a3c46f707d6f2dbbbc22f4e231b9
JuanOssa1 <juan.ossa2@u.icesi.edu.co>
2022-06-22 11:10:05 -0500

    Permuta y busca palabras

--- Punto5.py
+++ Punto5.py
@@ -1,14 +1,31 @@
-from urllib import response
-from urllib.request import urlopen
+from itertools import permutations
 import requests as rq
 import json
 
-URL="https://api.dictionaryapi.dev/api/v2/entries/en/hello"
-response= rq.get(URL, timeout=2.50)
-dict=response.json()
+word=input()
+#print([''.join(p) for p in permutations(word)])
 
-jsonString=json.dumps(dict)
+def searchWordInDictionary(word):
+    URL="https://api.dictionaryapi.dev/api/v2/entries/en/"+word
+    response= rq.get(URL, timeout=2.50)
 
-#jsonData = json.loads(response.text)
-jsonInList= json.loads(jsonString)
-print(jsonInList[0]['word'])
+    if(response.status_code==200):
+        dict=response.json()
+        jsonString=json.dumps(dict)
+        jsonInList= json.loads(jsonString)
+        return jsonInList[0]['word']   
+    else:
+        print("Esa palabra no existe")
+        
+
+def createListOfWords(word):
+    listOfPermutations=permutations(word)
+    for i in list(listOfPermutations):
+        wordToValidate="".join(i)
+        searchWordInDictionary(wordToValidate)
+
+createListOfWords(word)
+
+
+
+
