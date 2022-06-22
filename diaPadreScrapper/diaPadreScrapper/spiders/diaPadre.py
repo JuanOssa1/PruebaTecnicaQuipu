8bf555a2df992787451f3c10ed21619a5246cdf2
JuanOssa1 <juan.ossa2@u.icesi.edu.co>
2022-06-21 21:31:38 -0500

    Adding scraping conditional

--- diaPadreScrapper/diaPadreScrapper/spiders/diaPadre.py
+++ diaPadreScrapper/diaPadreScrapper/spiders/diaPadre.py
@@ -8,12 +8,18 @@ class DiapadreSpider(scrapy.Spider):
         #products= response.xpath("//h2[@class='ui-search-item__title']").extract()
         products= response.css(".ui-search-item__title.ui-search-item__group__element::text").extract()
         prices= response.css(".price-tag-fraction::text").extract()
-        
+        #discounts=response.css(".ui-search-price__discount::text").extract()
+        if response.css(".ui-search-price__discount::text"):
+            discounts=response.css(".ui-search-price__discount::text").extract()
+        else:
+            discounts="No tiene descuento"
 
-        for searched in zip(products,prices):
+        for searched in zip(products,prices,discounts):
+            
             searchedInfo={
                 'product': searched[0],
-                'price': searched[1]
+                'price': searched[1],
+                'discount': searched[2]
             }
 
             yield searchedInfo
